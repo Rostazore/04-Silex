9fc988fd8479119a45f363c713d0cd6ad85f1fb2
Rostazore <11044833+Rostazore@users.noreply.github.com>
2017-12-20 15:13:45 +0100

    20/12/2017 15:15.

--- src/TechNews/Controller/AdminController.php
+++ src/TechNews/Controller/AdminController.php
@@ -4,13 +4,98 @@
 namespace TechNews\Controller;
 
 
+use Silex\Application;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 
 class AdminController
 {
-    public function addarticleAction ()
+    public function addarticleAction (Application $app)
     {
-        return "<h1>Ajouter un article</h1>";
+        # Récupération de la liste des catégories.
+        $catégories = function () use ($app)
+        {
+            # Récupération des catégories dans la BDD.
+            $catégories = $app['idiorm.db']->for_table('categorie')->find_result_set();
+
+            # On formate l'affichage pour le champ select (ChoiceType).
+            $array = [];
+            foreach ($catégories as $catégorie) :
+                $array[$catégorie->LIBELLECATEGORIE] = $catégorie->IDCATEGORIE;
+            endforeach;
+
+            # On retourne le tableau.
+            return $array;
+        };
+
+        # Créer un formulaire permettant l'ajout d'un article.
+        $form = $app['form.factory']->createBuilder(FormType::class)
+            # Champ TITREARTICLE
+            ->add('TITREARTICLE', TextType::class, [
+                'required'      => true,
+                'label'         => false,
+                'constraints'   => [new NotBlank()],
+                'attr'          => [
+                    'class'         => 'form-control',
+                    'placeholder'   => 'Titre de  l\'article…'
+                ]
+            ])
+            # Champ IDCATEGORIE
+            ->add('IDCATEGORIE', ChoiceType::class, [
+                'choices'   => $catégories(),
+                'expanded'  => false,
+                'multiple'  => false,
+                'label'     => false,
+                'attr'      => [
+                    'class'     => 'form-control'
+                ]
+            ])
+            # Champ CONTENUARTICLE
+            ->add('CONTENUARTICLE', TextareaType::class, [
+                'required'      => true,
+                'label'         => false,
+                'constraints'   => [new NotBlank()],
+                'attr'          => [
+                    'class'         => 'form-control',
+                ]
+            ])
+            # Champ FEATUREDIMAGEARTICLE
+            ->add('FEATUREDIMAGEARTICLE', FileType::class, [
+                'required'      => false,
+                'label'         => false,
+                'attr'          => [
+                    'class'     => 'dropify'
+                ]
+            ])
+            # Champs SPECIALARTICLE et SPOTLIGHTARTICLE
+            ->add('SPECIALARTICLE', CheckboxType::class, [
+                'required'      => false,
+                'label'         => false,
+            ])
+            ->add('SPOTLIGHTARTICLE', CheckboxType::class, [
+                'required'      => false,
+                'label'         => false,
+            ])
+            # Bouton pour soumettre le villain bouton.
+            ->add('submit', SubmitType::class, ['label' => 'Publicationner'])
+
+            /**
+             * Maintenant que tous les champs ont été créés, nous allons pouvoir
+             * récupérer le formulaire.
+             */
+            ->getForm();
+
+            # Affichage du formulaire dans la vue.
+        return $app['twig']->render('admin/ajouterarticle.html.twig', ['form' => $form->createView()]);
     }
 }
--- src/app.php
+++ src/app.php
@@ -2,10 +2,21 @@
 
 
 use Silex\Provider\TwigServiceProvider;
-
 use Silex\Provider\AssetServiceProvider;
 use TechNews\Extensions\TechNewsTwigExtension;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\CsrfServiceProvider;
+use Symphony\Component\Translation\MessageSelector;
+use Symfony\Component\Translation\Translator;
+use Silex\Provider\LocaleServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Symfony\Component\Form\FormRenderer;
+/*use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Bridge\Twig\Form\TwigRenderer;
+use Symfony\Bridge\Twig\Form\TwigRendererEngine;*/
+
 
 
 # 1. Activation du débuggage.
@@ -41,5 +52,19 @@ require PATH_RESSOURCES . '/config/database.config.php';
 
 # 8. Sécurisation de l'application.
 require PATH_RESSOURCES . '/config/security.php';
+
+# 9. Importation pour les formulaires.
+$app->register(new FormServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new CsrfServiceProvider());
+$app->register(new LocaleServiceProvider());
+$app->register(new TranslationServiceProvider(), [
+    'translate.domains' => [],
+]);
+
+$app->extend('twig.runtimes', function ($array) {
+    $array[FormRenderer::class] = 'twig.form.renderer';
+    return $array;
+});
 
 return $app;
